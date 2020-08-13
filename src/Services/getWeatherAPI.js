2e625d5a1e81ec1bb3cc838bbe48a15776486d30
dg0397 <rgg0397@gmail.com>
2020-08-13 01:59:23 -0400

    adding struct to the app and wouter dependence

--- src/Services/getWeatherAPI.js
+++ src/Services/getWeatherAPI.js
@@ -6,7 +6,7 @@ export default function getWeatherAPI(latitude,longitude,setState,setStatus){
             const {coord,name, sys:{country},weather,main:{temp}} = location;
             const {main,icon} = weather[0];
             const weatherData = {coord,name,country,main,temp,icon};
-    
+            console.log(location)
             setState(weatherData)
             setStatus(false);
         })
