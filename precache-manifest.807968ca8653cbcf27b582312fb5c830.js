35115d03d69650d19ccd8109ea843f8f7746c161
dg0397 <rgg0397@gmail.com>
2020-08-16 02:10:11 -0400

    Updates

---
+++ precache-manifest.807968ca8653cbcf27b582312fb5c830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a1c4f9c342ec8aaebbf0fc2e6790c3",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "8f3dea30788707f1b6d9",
+    "url": "/React-Weather-App/static/css/main.8936b3d5.chunk.css"
+  },
+  {
+    "revision": "c79c59326091fb7fd348",
+    "url": "/React-Weather-App/static/js/2.3d247f0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.3d247f0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3dea30788707f1b6d9",
+    "url": "/React-Weather-App/static/js/main.72a96b0f.chunk.js"
+  },
+  {
+    "revision": "58ed85b682da3f6908da",
+    "url": "/React-Weather-App/static/js/runtime-main.b4c1cbc4.js"
+  }
+]);
