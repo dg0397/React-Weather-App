069cc7b7c55544938c0110c5627accf32d448ed2
dg0397 <rgg0397@gmail.com>
2020-08-17 22:44:17 -0400

    fixing a bug in the initial path

--- src/Components/Waiting/Waiting.jsx
+++ src/Components/Waiting/Waiting.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import useLocationContext from '../../hooks/useLocationContext';
 import './Waiting.css'
+import {useLocation} from 'wouter';
 
 export default function Waiting(){
     useLocationContext()
+    const [path,setPath] = useLocation();
+    if(path !== '/'){
+      setPath('/')
+    }
     return(
       <h1>Wait a minute...</h1>
     )
